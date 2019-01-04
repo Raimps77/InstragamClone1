fe8bd92f625ff232fe90b07edbdc7fa6446d4d05
Raimps77 <Raimps98731>
2019-01-04 19:00:36 +0200

    SignUp and Login

--- app/src/main/java/ee/raimo/instragamclone/SignUp.java
+++ app/src/main/java/ee/raimo/instragamclone/SignUp.java
@@ -1,5 +1,6 @@
 package ee.raimo.instragamclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -114,6 +115,10 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         btnTransition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent intent = new Intent(SignUp.this,
+                        SignUpLoginActivity.class);
+                startActivity(intent);
 
             }
         });
